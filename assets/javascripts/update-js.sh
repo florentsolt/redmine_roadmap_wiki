230fcb87abf7efcb496f9bda7bae687efd0498d8
Florent Solt <florent@solt.biz>
2016-04-13 10:34:46 +0200

    version bump

--- assets/javascripts/update-js.sh
+++ assets/javascripts/update-js.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 cd `dirname $0`
-curl 'https://github.com/florentsolt/roadmap.js/raw/master/roadmap.min.js' > roadmap.min.js
-curl 'http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.2/d3.min.js' > d3.min.js
+curl -L 'https://github.com/florentsolt/roadmap.js/raw/master/roadmap.min.js' > roadmap.min.js
+curl -L 'http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.2/d3.min.js' > d3.min.js
